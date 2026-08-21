2acdf769fc23958831fd8fd2b55f70720232767a
RVCE-DrSSN <shylashashi@gmail.com>
2026-08-21 23:51:22 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,6 +1,6 @@
 `default_nettype none
 
-module tt_um_MTK1234_anomaly_sentinel (
+module tt_um_Median_MAD (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
