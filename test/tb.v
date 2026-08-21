c425f94a1de381451155d9b6104c92ce15c592d8
RVCE-DrSSN <shylashashi@gmail.com>
2026-08-21 23:52:15 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_MTK1234_anomaly_sentinel user_project (
+  tt_um_Median_MAD  user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
